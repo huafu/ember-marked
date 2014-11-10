b7ed3c57a989217300860eee0e0f32586e20e12d
Huafu Gandon <huafu.gandon@gmail.com>
2014-11-10 11:05:07 +0700

    Updated example app

--- tests/dummy/app/routes/application.js
+++ tests/dummy/app/routes/application.js
@@ -1,6 +1,11 @@
 /* globals hljs */
 import Ember from 'ember';
 
+/* jshint -W101 */
+var MARKDOWN_SOURCE = "# ember-marked\n\nEmber component to easily render markdown (using `marked`, so GFM and syntax highlighting supported from scratch).\nA very basic example application using those components is available [there](http://huafu.github.io/ember-marked/).\n\n## Installation\n\n* `npm install --save-dev ember-marked`\n\n## Using\n\n* To render markdown content, simply use the component in your templates: `{{markdown-section someMarkdownProperty}}`\n* You also have access to a `code-section` component used to render syntax highlighted code: `{{code-section someSourceCode language=\'javascript\'}}`\n* You can look at the example application under `tests/dummy`, especially the `app/templates/application.hbs` showing a very simple example of using this addon.\n\n## Credits\n\n* Author: **[Huafu Gandon](http://github.com/huafu)**\n* This is using [marked](https://github.com/chjj/marked) and optionally [highlightjs](https://github.com/isagalaev/highlight.js) to render syntax highlighting sections\n";
+
+/* jshint -W101 */
+var CODE_SOURCE = "/* globals hljs */\nimport Ember from \'ember\';\n\n\nvar MARKDOWN_SOURCE = \'...\';\nvar CODE_SOURCE = \'...\';\n\nexport default Ember.Route.extend({\n  model: function () {\n    if (typeof hljs !== \'undefined\') {\n      return Ember.A(hljs.listLanguages());\n    }\n    else {\n      return Ember.A();\n    }\n  },\n\n  setupController: function (controller) {\n    this._super.apply(this, arguments);\n    controller.set(\'markdownSource\', MARKDOWN_SOURCE);\n    controller.set(\'codeSource\', CODE_SOURCE);\n  }\n});\n";
 
 export default Ember.Route.extend({
   model: function () {
@@ -14,7 +19,7 @@ export default Ember.Route.extend({
 
   setupController: function (controller) {
     this._super.apply(this, arguments);
-    controller.set('markdownSource', '# Some title\n---\nHello **you**!');
-    controller.set('codeSource', 'if (a === b) {\n  alert("hello");\n}');
+    controller.set('markdownSource', MARKDOWN_SOURCE);
+    controller.set('codeSource', CODE_SOURCE);
   }
 });
